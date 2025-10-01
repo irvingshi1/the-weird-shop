84bf23f284117487dc943527515d5be37122aad0
Irving Shi <ishi@terpmail.umd.edu>
2025-09-30 22:12:21 -0400

    Made a functioning receipt tab.

--- the_weird_shop/lib/user_data.dart
+++ the_weird_shop/lib/user_data.dart
@@ -6,11 +6,21 @@ class UserData extends ChangeNotifier {
   final List<Item> _itemList = <Item>[
     Item('Tap Water', 0, 0),
     Item('Iron', 0.01, 0),
+    Item('Paper', 0.1, 0),
+    Item('Silk', 20, 0),
     Item('Silver', 200, 0),
-    Item('Gold', 10000, 0),
+    Item('Saffron', 3000, 0),
+    Item('Rubidium', 6000, 0),
+    Item('Platinum', 12000, 0),
+    Item('Gold', 15000, 0),
+    Item('Diamond', 35000, 0),
     Item('Rhodium', 500000, 0),
+    Item('Sapphire', 1000000, 0),
+    Item('Ruby', 1500000, 0),
+    Item('Anti-matter', 50000000, 0),
     Item('Oganesson', 75000000, 0),
     Item('Unicorn Powder', 250000000, 0),
+    Item('Alien Alloy', 600000000, 0),
   ];
 
   final List<Receipt> _receiptList = <Receipt>[];
